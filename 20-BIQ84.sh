1894806b0f03cbe5ea9feaf40966f3e1711c6cc5
learnf5 <99698691+learnf5@users.noreply.github.com>
2025-04-17 12:46:37 -0400

    Update 20-BIQ84.sh

--- 20-BIQ84.sh
+++ 20-BIQ84.sh
@@ -2,4 +2,5 @@
 for i in {1..12}; do [ "$(sudo ssh root@192.168.1.31 cat /var/prompt/ps1)" = "Active" ] && break; sleep 5; done
 
 # confirm bigiq1 is active
-for i in {1..12}; do [ "$(sudo ssh root@192.168.1.51 cat /var/prompt/ps1)" = "Active" ] && break; sleep 5; done
+###need to run ssh-copy-id on jump for this to work; commenting out for now; will fix later
+###for i in {1..12}; do [ "$(sudo ssh root@192.168.1.51 cat /var/prompt/ps1)" = "Active" ] && break; sleep 5; done
