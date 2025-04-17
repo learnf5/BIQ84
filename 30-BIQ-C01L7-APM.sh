db10d89024a267d0cc3c4884cbb6e5878a528912
learnf5 <99698691+learnf5@users.noreply.github.com>
2025-04-17 15:20:27 -0400

    Update and rename 20-BIQ-C01L7-APM.sh to 30-BIQ-C01L7-APM.sh

---
+++ 30-BIQ-C01L7-APM.sh
@@ -0,0 +1,4 @@
+# download config from GitHub and copy and load/merge to bigip1
+curl --silent https://raw.githubusercontent.com/learnf5/big-iq/main/avr.scf --output /tmp/avr.scf
+sudo scp /tmp/avr.scf 192.168.1.31:/var/local/scf
+sudo ssh 192.168.1.31 tmsh load sys config merge file avr.scf
