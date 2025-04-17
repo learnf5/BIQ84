6f1455f9290cab2151eee858b671b32990d86645
learnf5 <99698691+learnf5@users.noreply.github.com>
2025-04-17 18:09:00 -0400

    Delete 30-BIQ84-C01L7-APM.sh

--- 30-BIQ84-C01L7-APM.sh
+++
@@ -1,4 +0,0 @@
-# download config from GitHub and copy and load/merge to bigip1
-curl --silent https://raw.githubusercontent.com/learnf5/$COURSE/main/avr.scf --output /tmp/avr.scf
-sudo scp /tmp/avr.scf 192.168.1.31:/var/local/scf
-sudo ssh 192.168.1.31 tmsh load sys config merge file avr.scf
