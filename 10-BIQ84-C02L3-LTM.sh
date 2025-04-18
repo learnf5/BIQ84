46be717edd86d1db81948a63e15d3b3858826317
learnf5 <99698691+learnf5@users.noreply.github.com>
2025-04-17 22:34:04 -0400

    Update 10-BIQ84-C02L3-LTM.sh

--- 10-BIQ84-C02L3-LTM.sh
+++ 10-BIQ84-C02L3-LTM.sh
@@ -1,5 +1,5 @@
 # grab student files from GitHub 
 labdir=/home/student/Desktop/Lab_Files
 mkdir $labdir
-curl --silent --remote-name-all --output-dir $labdir/ https://raw.githubusercontent.com/learnf5/$COURSE/main/cut_and_paste.txt
-curl --silent --remote-name-all --output-dir $labdir/ https://raw.githubusercontent.com/learnf5/$COURSE/main/expired_certificate.crt
+curl --silent --remote-name-all --output-dir $labdir/ https://raw.githubusercontent.com/learnf5/$COURSE_ID/main/cut_and_paste.txt
+curl --silent --remote-name-all --output-dir $labdir/ https://raw.githubusercontent.com/learnf5/$COURSE_ID/main/expired_certificate.crt
