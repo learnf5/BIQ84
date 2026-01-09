55bab65c248e896cf728863ebcd3683e3d3fd37a
F5 Education Services <99698691+learnf5@users.noreply.github.com>
2026-01-08 21:14:33 -0500

    Comment out AS3 template upload command
    Commented out the SSH command for posting AS3 template.

--- 30-BIQ84-C01L1-AS3.sh
+++ 30-BIQ84-C01L1-AS3.sh
@@ -2,7 +2,7 @@
 curl --silent https://raw.githubusercontent.com/f5devcentral/f5-big-iq/refs/heads/7.1.0/f5-appsvcs-templates-big-iq/default/json/AS3-F5-HTTP-lb-template-big-iq-default.json --output /tmp/AS3-F5-HTTP-lb-template-big-iq-default.json
 ssh-keyscan -H 192.168.1.51 >> ~/.ssh/known_hosts
 sshpass -p f5trn001 scp /tmp/AS3-F5-HTTP-lb-template-big-iq-default.json root@192.168.1.51:/var/tmp/AS3-F5-HTTP-lb-template-big-iq-default.json
-sshpass -p f5trn001 ssh root@192.168.1.51 curl -skH "Content-Type: application/json" http://localhost:8100/cm/global/appsvcs-templates -X POST -d @/var/tmp/AS3-F5-HTTP-lb-template-big-iq-default.json | jq .
+###sshpass -p f5trn001 ssh root@192.168.1.51 curl -skH "Content-Type: application/json" http://localhost:8100/cm/global/appsvcs-templates -X POST -d @/var/tmp/AS3-F5-HTTP-lb-template-big-iq-default.json | jq .
 
 # download config from GitHub and copy and load/merge to bigip1
 curl --silent https://raw.githubusercontent.com/learnf5/$COURSE_ID/main/avr.scf --output /tmp/avr.scf
