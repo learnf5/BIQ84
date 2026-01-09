d4b5276d4a6ba7c63b5e25833d38ea1982ca953c
F5 Education Services <99698691+learnf5@users.noreply.github.com>
2026-01-08 20:05:18 -0500

    Modify AS3 script to include sshpass for secure transfer
    Updated SSH and SCP commands to use sshpass for authentication.

--- 30-BIQ84-C01L1-AS3.sh
+++ 30-BIQ84-C01L1-AS3.sh
@@ -1,7 +1,8 @@
 # download AS3 template from GitHub and load into bigiq1
 curl --silent https://raw.githubusercontent.com/f5devcentral/f5-big-iq/refs/heads/7.1.0/f5-appsvcs-templates-big-iq/default/json/AS3-F5-HTTP-lb-template-big-iq-default.json --output /tmp/AS3-F5-HTTP-lb-template-big-iq-default.json
-sudo scp /tmp/AS3-F5-HTTP-lb-template-big-iq-default.json 192.168.1.51:/var/tmp/AS3-F5-HTTP-lb-template-big-iq-default.json
-sudo ssh curl -s -H "Content-Type: application/json" http://localhost:8100/cm/global/appsvcs-templates -X POST --upload-file /var/tmp/AS3-F5-HTTP-lb-template-big-iq-default.json | jq .
+ssh-keyscan -Н 192.168.1.51 >> ~/.ssh/known_hosts
+sshpass -p f5trn001 scp /tmp/AS3-F5-HTTP-lb-template-big-iq-default.json root@192.168.1.51:/var/tmp/AS3-F5-HTTP-lb-template-big-iq-default.jsoก
+sshpass -p f5trn001 ssh root@192.168.1.51 curl -skH "Content-Type: application/json" http://localhost:8100/cm/global/appsvcs-templates -X POST -d @/var/tmp/AS3-F5-HTTP-lb-template-big-iq-default.json | jq .
 
 # download config from GitHub and copy and load/merge to bigip1
 curl --silent https://raw.githubusercontent.com/learnf5/$COURSE_ID/main/avr.scf --output /tmp/avr.scf
